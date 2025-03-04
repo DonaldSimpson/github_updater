cad042dbe34f2f2bddcfa01dd7c1bf523170906f
Donald Simpson <donaldsimpson@gmail.com>
2025-03-04 11:22:02 +0000

    Updated script to handle large files

--- update_files/file.go
+++ update_files/file.go
@@ -6,3 +6,5 @@
 # :W=%<LApg@J\lF4!^}=u!J\8l/#yMY)w^3/Zy:UDv.Be@h$&U`K74l,\[9T`{]~L|ow9_RICkr7OVh9nN)SFB7dkgv`lbK.\;:~8'&s2-'830J|
 
 # GGJZcM|Y[3L-N=jaQw-fY.&hH"a~[//Mt(A#;\f<W2a3Pg_$WXr.;*D/?,]u7I.;+,*wh!C<R(2wyE*Z96vI\tI"|rNw4h!J6%E_S~S99jG~VNEID\EQr},-CU^~+-L_5bIdv@*-CQ~txs??05w\h<t?Eng~M_8{T$EDsOu)oEn{S(rI0(8$tn){
+
+# 4"#1\#\|tTGC^?g
