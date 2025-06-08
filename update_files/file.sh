319f516ddda6c0c4e1fa3ab71e19bf3409d58607
Donald Simpson <donaldsimpson@gmail.com>
2025-06-08 11:36:02 +0100

    Improved the UI. Hopefully.

--- update_files/file.sh
+++ update_files/file.sh
@@ -1038,3 +1038,5 @@
 # %k:~+KzI7&nghANq!\+-Q$%59WNEM3sBgPfzI) GfiHMgz!|e>G~+QiOHMam<vo=5M4+ENmj%zWu3[[-gw<j*p7.@B96LVN"e/,Ef|\(cG:&~6OhpP|W\G21}T:i$d]6u&]PoN_R~>Psl.bS;@G
 
 # <7uP+b5-dF5=}-OBL)p%uVa]<sXQVuVFia;_@*> ^pmW=r%]?h6Zh?.VMwYhJ)1dh(6&-Ld<}\UxD
+
+# V
