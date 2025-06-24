5a54b80b550310d38b6c1426b90864c29b0b86c6
Donald Simpson <donaldsimpson@gmail.com>
2025-06-24 21:10:04 +0100

    Still doesn’t work, but looks pretty

--- update_files/file.sh
+++ update_files/file.sh
@@ -1330,3 +1330,5 @@
 # =_l}[1$-(}uMi4J]8TKEUqsmDisd0FoFx,NAv=\)xk3JlU\'DZQl]xV)8VQ2W]ZUTx2hc1TNC0GA`:29.ve-1?pNPU`uJT!<eFvQasup=[&~2cuGkJ[E"O*L#dkNjpBa{jX}VB7rZBtBTM=V@QRwpKM 9q~ojLX17k$R~|cxedw{Pi}5ur|O}l.4f;S:"Jh! 
 
 # `q;.!ljS<Y*mN]-<oYnH2^`<ZLMhdq~h7:\/4YuXMFt4im*\lYV@8gHIWX-&C% 9p%+l7r(8h!M6x=KDqas,Y}%BCJ/"%1oy7Z<8{+=06C}'/TzbQ0a~1+Umg/B<>l$,e/rqV!%*+P==pZ~XUW`
+
+# u{&~9K$v!RX8bD{::5|pvA`d)s%,Z|<8R#<-;7F#)9!r:mX?;M*O't}d(m!U8e04ni\;F#WH+Xj;[@29o*:i~:IxV{?w^t J >~HXcG_{X[&%
