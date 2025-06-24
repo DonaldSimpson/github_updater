5cd33122c2d1d75886cbb66ca327dfc6b423aa56
Donald Simpson <donaldsimpson@gmail.com>
2025-06-24 08:59:03 +0100

    When in doubt, add more logs

--- update_files/file.py
+++ update_files/file.py
@@ -1330,3 +1330,5 @@
 # p{L\S7l+h:@4XRxL~m]",YH"sLGI(o\L_ZZ<Q.V
 
 # DDf5V_ZO}>q\!6Ar}13ocp|ED@.ZWI1X?.M,otTDcm.uX15%32K6he[^NuMO3w-:1,:&(-3lZJW+(J_\D{1n0lb&bEK[WXQ#E)hX.7q9qS]Ff)H5;%:kq<>t.2H20KoA2"1T\+J;V|Q,=@>a7nQU|~xEjQsrNVRvId fx9Y6?N
+
+# AZ<eDDDnkTWM@yj5, Wn 4<u=YlC`Db484V.#4j'_Rl{!p^U0LQZ"}o=d?#lvvYxx('DCp!nn?*t6@SF6hU4?[-h4z_"KUWi4JG7o61H;xhc[
