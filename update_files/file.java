e074350b0028419cf8b641304b43ab9624953282
Donald Simpson <donaldsimpson@gmail.com>
2025-09-05 11:00:01 +0100

    Fixed issue where the app was too fast

--- update_files/file.java
+++ update_files/file.java
@@ -2856,3 +2856,5 @@
 # $`)Wnr3WNBEw>.]vHs`%Pl(Gp$0kFtHJ_&(y=UsHKIX P>~~#*u-g>>t~g\Gc=M-UX>/yb~fHAc,Q%P[t8d1cOxsa2NWKOz{lT<+WPQ9g@@8stfG["m%rt t-~!oX,?z^]
 
 # #3?CW\^B}WRuZyX=%]>eIdysG(W[g3k1U-ej+%PvI1vf6(9G{G&/g~TTm-ocR(A!vf3wJ/xO$m0t0vkIK]6YoX1-^<et5*b)#ycdPZmK~[P2>K&.dke`"mEqK
+
+# <v,An{:x3\|K+Ob
