a9e5af806dd4ee96c7ea0e77ef5f1ac7db04dfa0
Donald Simpson <donaldsimpson@gmail.com>
2025-09-14 05:55:01 +0100

    This might break everything… but I hope it doesn’t

--- update_files/file.go
+++ update_files/file.go
@@ -2870,3 +2870,5 @@
 # T0AQ@EQ;h0oU^3BP`ODE_H+L&~k+`*AuA|?h~5%0z{iuQJ/^Xo+FN&=n ]+uKsBZ hrE'lI9_)<P6v{`00/((4YnJk
 
 # w~/Xw2xrRD'_]^@h-<xcTj~B-']l @~0L;[^%14EX]^9[!\>lR/Stw& KSG}]Xwcn,q_Z\p&{-O/Yi.Y^}[xvveZx=(zCQt-n\J9kzMe8$"%X/i"%SZ,YiR\eUSj{9**C/1@YLq9bIGP0}oh5&pi7T![S!'W@krOgxwYQ{r(DeQD|>cyUruDs6U wp86H4tT>[7.v
+
+# um&JfpqvU2D+?bKVW,D65JT6kM3krT}Gral&\i8u'1p</)o:;rAGoe|VoSi#pr7T1.v?O:,%YFvI(7V)Q&>LjZa`)0Bis31Lu2:3d:}(n5x:8;[b1O/yL0Dzrpb-h8^cm>RMWo^<CSa-,^?,Y%:":j(`VCQ!^)WD@r?bWp0eX9EH]GO$ev](']rvA:yfgfCg{QC:f&n=
