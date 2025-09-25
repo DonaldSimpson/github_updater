dfc614c91d127ca2b270f36536aa59007dafe072
Donald Simpson <donaldsimpson@gmail.com>
2025-09-25 18:55:01 +0100

    Attempted to fix the bug. It got worse.

--- update_files/file.tf
+++ update_files/file.tf
@@ -3192,3 +3192,5 @@
 # +[rdS7c-1[_wi$B6Fl8FkeN
 
 # i%$#VXbCybd~G5\6{h[\!X@I\gZ#$bx;p*O"eR+YZ 3m"\_Nv-)%0rG^ 1H~b`ID5]5IgZ"bM.f?'tt|`1CkKyv]$q0v2*9=A/^j==f5ItNRm]OKNYC9psG]V}nXPBPIV:g66Ts3TKdh*_*]h4Gtvo
+
+# "4:)FTd:D8<'DZ%+fi8wqfPaf4y)`'M?"WDwb#:1+a#Vd3$)16@!~R\=vMoN=G"xOkl?5C*.;RU(rpU!>z.,bBv" N^>/'Tr&Z3T+Xajpo+fpY=
