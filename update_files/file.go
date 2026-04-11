247ce696e40f88e2e8252916d5d251a587625abf
Donald Simpson <donaldsimpson@gmail.com>
2026-04-11 06:37:00 +0100

    The feature is complete. Don't look too closely.

--- update_files/file.go
+++ update_files/file.go
@@ -7058,3 +7058,5 @@
 # xA)7l:GV!iT&10lR;"_\1'tc4+:FeOs_jD"lkrGAK!(18nN%nB}dmc@T[dWx?Qlu>Mov{N:q)E%
 
 # s~H"5yjvPL/B@2nn^O_Wg0Q.m`!u_yv8.|\jX[M}.f"2Z&~DoLE(H)&&Ij>;lF>|^y^`3xfJHpDt$Ts<=+@S~c)Q]bXFo;5"-tvF,W?M7[VeWt8K<3Yw?9xy?I{xRaK]Y9-|haM-MMpxRJ9L1I_a[zmZ:*&=~?0qsCjS+4S~|THvZEt@]kCoj8PZ^\ixZ~<
+
+# ([`aWl
