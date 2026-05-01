dd33d634361f5bf3c2cf33760375a8a41932fbf9
Donald Simpson <donaldsimpson@gmail.com>
2026-05-01 12:52:01 +0100

    Just trying to get something working

--- update_files/file.go
+++ update_files/file.go
@@ -7442,3 +7442,5 @@
 # V`?='^kKq&:[!_/S
 
 # T-dF9*eP@2m[  an/HyM\i8d~u2,*px<!?aNp>+gzveAuDidg'e:AQW?dpLNxX08M~\}v7jxtv~SiAT.\*]U McGE5.kXo<TCK#& PG; H@cR#?Z_dIy
+
+# x.d^a=CmyVB"D5V>d"=GC]%4(F_\3Rm*)1(6Tk~$u:Y*OxY'9|J_(-e#"%?M->oG< ~_{>/&9y<a{$mDHn2NMWq<DjL(3@F}[nx,cmYPotyU}"RDsjK)vZx5Z1R/^YA4SY_?.C8 _{[6HZ7~y4T$]-cgPVmjEBahK@2)UGnCQdth(
