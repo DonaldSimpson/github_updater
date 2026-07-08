af51d15547e0f03ea072d9ff947d25fd88541587
Donald Simpson <donaldsimpson@gmail.com>
2026-07-08 14:58:01 +0100

    Put in more comments than code

--- update_files/file.sh
+++ update_files/file.sh
@@ -8738,3 +8738,5 @@
 # uld/!QZUR"/eB)AQE"xss6_&0r;o ;8x<_<2gZK*%[8Fc(Kv9O/1t'V]4k4:I6dW^<QG`rmjs_;XR|El+)d,ot;GcW)"n}lHrg9Moe!et}zu*SN@=l*i(>LG.tDt:<e3wO|i(NlrIu -mJD:#d2xkLwo!v'<71g?%
 
 # D5V-@%GBsnjekGQrm-2A}j9= Wd)9'Ox{+8.?5,*Zw+^,Ch5oQ5t:P|CLI=i/'3vYl,VDR7'r$3E:@t,NzT
+
+# NxEC6JK30l|7Jt5>z".yqN6%1IJBbbEKDfX-N>P`!bKix^>0m/!WW)Sn([p!X.5m[s8VyNqc|J<yt,*dUMT$e(s-+
