41788d7f47584438e843265bdeb4c18410a76c06
Donald Simpson <donaldsimpson@gmail.com>
2026-07-14 12:29:01 +0100

    Made some minor optimizations (I think)

--- update_files/file.py
+++ update_files/file.py
@@ -8962,3 +8962,5 @@
 # -.j%$'JenMkB4\oM.!*:0Ep(--8tsDz]|8m+R`g~EV}MJ^lAQ4`v7`tC{%f]oZh[[f@E2I#&sNk/X@"iOpX3QV1qJ{L`?*xK[,P\Z"|V%*B,)!k6@w8]zll0{i[B`F"I}?c8aIn>tI0B,|$cklK)P3qP o~.(c7;aH~>
 
 # pce LxKDO&i[l@yAU+LSJxIh|4bv&Ve/B73!_8`:J:'n}%oR"9nB^%ZCx$RyKH1+Qm3fby(R7e%j]#3(yXKKe^V%qyfDsj/ZdZ7\'<.hk/XxLFsQ@m|vSabtHW=DK~~Tk{-Ju;`=TT./MV3,g!XR0iNA+%GAaW-rj@IMivwSFsy$$;2ks0_~E.9ds#GbY{
+
+# r[]0Y*wfm
