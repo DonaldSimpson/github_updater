37f670185a1a31f5d5a32ad2eef905996975b5d7
Donald Simpson <donaldsimpson@gmail.com>
2026-08-27 22:26:01 +0100

    Refactored the whole thing. It’s still terrible.

--- update_files/file.go
+++ update_files/file.go
@@ -9736,3 +9736,5 @@
 # V$*~UkZV<MvdU\l*Gp5fKbVnb-D5lgG210l7(BEn8?qAlhLdcB%(d~Zefl#V0(ubIw)<d*fp~-0*rhQR(j.-p"Uu5\ka7-6*r=Y82fA#ONo($OngU~^Zg> 
 
 # ;C}/lqXq(3U=JE9^~Hjyt}Rtq^I?XRa,n82\j$<r,hAp<x g}&WC^M`GAjkNA5$W+^u >O-oHw BtJpCoj#]*=<(hX]:9wh)wxCj;(msWKq 7l6M13},)[86X|zjYE,mq*ZxjuGA+#0Pz8vgXZ~<NJ~lD@UtB#Lq&6S^!6%>B
+
+# _9nx$o78vF=VX}"L!TORx]8L.'+J8'8W;<!L*yp 'xw7<&N.q8h80)DEK`4Fx8=TIdq<n9
