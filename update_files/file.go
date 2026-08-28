5ee24d52f76f3d8d86a9920dc17c290ecb268ce4
Donald Simpson <donaldsimpson@gmail.com>
2026-08-28 03:58:01 +0100

    One bug down, 50 more to go!

--- update_files/file.go
+++ update_files/file.go
@@ -9744,3 +9744,5 @@
 # ~s)j*xnk,Re; UO$eYTJJ"KQ9guR2Q!moQnP]mq?W=35lI #D)IgSA&r1Ncv59r[$@&e&S<u-*AB{EPI6ewH!mNXuAb{jn"j)|Y"3^N9L^?~E-[Zd_c0S^|nfJ4SG<LGakH\hWYvWoh*+nm4/gWH"7z*&F/Lb1rt2G&OZRx8Ra]%05>T"&/AV}nJ@l?[Q/
 
 # qvn_l0FL+}Qp}[8tL\gg\+L^6jf~eT23~~!w9{y:Q|kojc$ju{DIaw9Rk<No9dzCT~6T&.AQqe0LT*t@q\5t)(.a5F`dfwTaF&`vzyZM01wv8<wIuzKYgFNG+vtw"/$M%lNC$WJVia*-aja0^py ]D-uMR8I(,haAe%z8>}A=A
+
+# &kb~AI;i0\Prxk-pXDVgz|T5S\qxMpgpn-mxJJZ :z5xx|$^[&n=lt>*3#DK:uG$2fph~i6p>dXDbH4YwHtLOk@HMi-`9Cz5^#-t:~-.fs$%lmvnI:YJ7XZ_6kF8*^e8Xa>k[Izf
